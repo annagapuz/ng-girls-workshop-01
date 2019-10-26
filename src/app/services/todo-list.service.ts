1025fdda9063aeb6b0c4dd7fb0b6981f06b5d812
Anna Gapuz <amgapuz@gmail.com>
2019-10-26 15:32:10 -0400

    Finished chapter 17

--- src/app/services/todo-list.service.ts
+++ src/app/services/todo-list.service.ts
@@ -1,21 +1,28 @@
 import { Injectable } from '@angular/core';
 import {TodoItem} from "../interfaces/todo-item";
+import {StorageService} from "./storage.service";
+
+const todoListStorageKey = 'Todo_List';
+
+const defaultTodoList = [
+  {title: 'install NodeJS'},
+  {title: 'install Angular CLI'},
+  {title: 'create new app'},
+  {title: 'serve app'},
+  {title: 'develop app'},
+  {title: 'deploy app'},
+];
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoListService {
 
-  private todoList: TodoItem[] = [
-    {title: 'install NodeJS'},
-    {title: 'install Angular CLI'},
-    {title: 'create new app'},
-    {title: 'serve app'},
-    {title: 'develop app'},
-    {title: 'deploy app'},
-  ];
+  todoList: TodoItem[];
 
-  constructor() { }
+  constructor(private storageService:StorageService) {
+    this.todoList = storageService.getData(todoListStorageKey) || defaultTodoList;
+  }
 
   getTodoList() {
     return this.todoList;
@@ -23,6 +30,27 @@ export class TodoListService {
 
   addItem(todoItem: TodoItem) {
     this.todoList.push(todoItem);
+    this.saveList();
   }
 
+  // We locate the item in the list. Then in the same place we assign a new object,
+  // which is constructed from the original item and the changes made to it.
+  // We're using the spread operator for this: a new object is constructed, composed of the
+  // original set of keys and values (...item) which are overridden by the keys and values of
+  // changes. (If a key in changes doesn't exist in item, it is added to the new object.)
+  updateItem(item: TodoItem, changes) {
+    const index = this.todoList.indexOf(item);
+    this.todoList[index] = { ...item, ...changes };
+    this.saveList();
+  }
+
+  deleteItem(item: TodoItem) {
+    const index = this.todoList.indexOf(item);
+    this.todoList.splice(index, 1);
+    this.saveList();
+  }
+
+  private saveList() {
+    this.storageService.setData(todoListStorageKey, this.todoList);
+  }
 }
