2fb92317bac33688ace48c68c50a30b442cabd08
Anna Gapuz <amgapuz@gmail.com>
2019-10-26 15:13:49 -0400

    Finished chapter 15

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { InputButtonUnitComponent } from './input-button-unit/input-button-unit.component';
-import { TodoItemComponent } from './todo-item/todo-item.component';
-import { ListManagerComponent } from './list-manager/list-manager.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {InputButtonUnitComponent} from './input-button-unit/input-button-unit.component';
+import {TodoItemComponent} from './todo-item/todo-item.component';
+import {ListManagerComponent} from './list-manager/list-manager.component';
+import {TodoListService} from "./services/todo-list.service";
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { ListManagerComponent } from './list-manager/list-manager.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [TodoListService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
